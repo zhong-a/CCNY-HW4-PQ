86558669bd8466b3b1d0f3fda180fe7c613b8a53
Alan Zhong <alan.zhong.51@gmail.com>
2019-06-27 16:53:11 -0400

    Added push() function to PriorityQueue

--- src/hw4q3.cpp
+++ src/hw4q3.cpp
@@ -19,14 +19,35 @@ PriorityQueue<Item>::PriorityQueue(const PriorityQueue<Item>& source) {
     last = source.last;
     used = source.used;
     size_t index = first;
-    for (index; index < used; index = nextIndex(index)) {
+    for (int i = 0; i < used; i++) {
         data[index] = source.data[index];
+        index = nextIndex(index);
     }
 }
 
 template <class Item>
 PriorityQueue<Item>::~PriorityQueue() {
     delete[] data;
+}
+
+template <class Item>
+void PriorityQueue<Item>::push(const Item& entry) {
+    if (used == capacity) {
+        return;
+    }
+    data[nextIndex(last)] = entry;
+    size_t index = last;
+    Item holder;
+    size_t iter = 0;
+    while (data[index] > data[nextIndex(index)]) {
+        if (nextIndex(index) == first) {
+            break;
+        }
+        holder = data[index];
+        data[index] = data[nextIndex(index)];
+        data[nextIndex(index)] = holder;
+        index = prevIndex(index);
+    }
 }
 
 
